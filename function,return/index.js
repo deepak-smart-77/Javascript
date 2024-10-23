e2ba952cced4970e1869035ec18f2e8bc066551d
deepak-smart-77 <deepaksneha07@gmail.com>
2024-10-23 10:56:56 +0530

    day 6

---
+++ function,return/index.js
@@ -0,0 +1,16 @@
+// function happyBirthday(userName,age){
+//     console.log("happy birthday to",userName ,"..You r",age,"years old");
+// }
+
+// function variable(){
+//     let userName = "deepak";
+//     let age = 20;
+
+//     happyBirthday(userName,age);
+// }
+
+// variable();
+
+
+
+
