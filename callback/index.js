e303d05e95ab23fedf7f1af0cf0d3a05f27176ad
deepak-smart-77 <deepaksneha07@gmail.com>
2025-01-31 08:52:09 +0530

    jan 30

--- callback/index.js
+++ callback/index.js
@@ -26,16 +26,63 @@
 //-----array.foreach----
 
 
-let students = ["deepak", "raghul", "cibi"]
-students.forEach(capitalize)
-students.forEach(print)
+// let students = ["deepak", "raghul", "cibi"]
+// students.forEach(capitalize)
+// students.forEach(print)
 
-function capitalize(element, index, array){
-    array[index] = element[0].toUpperCase() + element.substring(1);
+// function capitalize(element, index, array){
+//     array[index] = element[0].toUpperCase() + element.substring(1);
 
-}
-console.log(students[0]);
+// }
+// console.log(students[0]);
 
-function print(element){
-    console.log(element);
+// function print(element){
+//     console.log(element);
+// }
+
+
+//-------array.map----
+
+
+// let numbers = [1, 2, 3, 4, 5];
+// let squares = numbers.map(square);
+
+// squares.forEach(print);
+
+// function square(element){
+//     return Math.pow(element,2);
+// }
+
+// function print(element){
+//     console.log(element);
+// }
+
+// console.log(squares[3]);
+
+
+//-----array.filter()---
+
+
+// let ages = [14, 18, 19, 21, 31, 17, 42];
+// let adults = ages.filter(checkAge);
+
+// adults.forEach(print);
+
+// function checkAge(element){
+//     return element >= 18;
+// }
+
+// function print(element){
+//     console.log(element);
+// }
+
+//----array.reduce()----
+
+let prices = [ 12, 24, 67];
+let totalPrice = prices.reduce(add);
+
+console.log(totalPrice);
+
+function add(total, element){
+    return total + element;
 }
